efcab149c393f18d724c0a18e12a7d6a2e01a156
Zachary Cook <zcook04@gmail.com>
2024-01-09 10:22:13 -0500

    Update doc strings

--- backend/lib/game/ChessGame.py
+++ backend/lib/game/ChessGame.py
@@ -1,6 +1,7 @@
 import sys
 import os
 import uuid
+import json
 
 ## Temp for testing.
 sys.path.append(os.path.abspath(os.path.join(
@@ -28,25 +29,42 @@ class ChessGame:
         self.coordinates = self.initialize_coordinates()
         self.initialize_pieces()
         self.id = uuid.uuid4()
+        self.game_moves = []
+        
+    def __repr__(self):
+        return json.dumps({
+            'uuid': self.id,
+            'game_moves': self.moves,
+            'board': self.board,
+        })
+        
 
     def initialize_board(self) -> list[list]:
-        '''Returns a chessboard represented by a list of lists with
-        None as placeholders for pieces.'''
+        """Returns a chessboard represented by a list of lists with
+        None as placeholders for pieces.
+
+        Returns:
+            list[list]: A list of lists (8x8) containing either None or a ChessPiece
+        """
         board = []
         for _ in range(8):
             board.append([None for _ in range(8)])
         return board
 
     def initialize_coordinates(self) -> dict:
-        '''Returns a dictionary of each square in chess notation as a
-        key with the board coordinates as the value: i.e., a1: (0, 0)'''
+        """Returns a dictionary of each square in chess notation as a
+        key with the board coordinates as the value: i.e., a1: (0, 0)
+
+        Returns:
+            dict: chess algebriac notation mapped to board coordinates.
+        """
         numbers = [x for x in range(1, 9)]
         letters = [chr(i) for i in range(ord('a'), ord('h')+1)]
         return {(letter + str(number)): (8 - number, ord(letter) - ord('a'))
                 for number in numbers for letter in letters}
 
     def initialize_pieces(self) -> None:
-        '''Place chess pieces on their starting squares.'''
+        """Place chess pieces on their starting squares."""
         white_pawn_squares = ['a2', 'b2', 'c2', 'd2', 'e2', 'f2', 'g2', 'h2']
         black_pawn_squares = ['a7', 'b7', 'c7', 'd7', 'e7', 'f7', 'g7', 'h7']
         for square in white_pawn_squares:
@@ -71,24 +89,46 @@ class ChessGame:
         self.place_piece(BlackQueen(self.coordinates['d8']), 'd8')
 
     def place_piece(self, chess_piece: ChessPiece, chess_square: str) -> None:
-        '''Places a ChessPiece on the square.  chess_square should be entered as
-        a chess notation, ie a1, or d4.'''
+        """Places a ChessPiece on the chess_square.
+
+        Args:
+            chess_piece (ChessPiece): A class inheriting from ChessPiece.
+            chess_square (str): chess algebreric notation ie. "a1"
+        """
+        ''''''
         column, row = self.coordinates[chess_square.lower()]
         self.board[row][column] = chess_piece
 
     def remove_piece(self, chess_square: str) -> None:
+        """Removes a piece from the chessboard.
+
+        Args:
+            chess_square (str): chess algebreic notation ie. "a1"
+        """
         column, row = self.coordinates[chess_square.lower()]
         self.board[row][column] = None
 
     def occupying_piece(self, chess_square: str) -> ChessPiece | None:
-        '''Returns a ChessPiece if coordinates is occupied otherwise returns None'''
+        """Returns a ChessPiece if coordinates is occupied otherwise returns None
+
+        Args:
+            chess_square (str): chess algebreic notation ie. "a1"
+
+        Returns:
+            ChessPiece | None: returns class inheriting from ChessPiece | None.
+        """
         column, row = self.coordinates[chess_square.lower()]
         if self.board[row][column]:
             return self.board[row][column]
 
-    def move_piece(self, starting_square, ending_square):
-        '''Moves a piece from a starting square to ending square if
-        piece exists at the starting square and is a legal move.'''
+    def move_piece(self, starting_square: str, ending_square: str) -> None:
+        """Moves a piece from a starting square to ending square if
+        piece exists at the starting square and is a legal move.
+
+        Args:
+            starting_square (str): chess algebreic notation ie. 'a1'
+            ending_square (str): chess algebreic notation ie. 'a1'
+        """
         piece_to_move = self.occupying_piece(starting_square)
         piece_at_destination = self.occupying_piece(ending_square)
         if not piece_to_move:
