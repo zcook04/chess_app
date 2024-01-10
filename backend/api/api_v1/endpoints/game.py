7b86e6403c886e9267542b1828375a88ddd36695
Zachary Cook <zcook04@gmail.com>
2024-01-09 21:17:18 -0500

    Add board return from /newgame

--- backend/api/api_v1/endpoints/game.py
+++ backend/api/api_v1/endpoints/game.py
@@ -1,9 +1,12 @@
 from fastapi import APIRouter
 from fastapi.responses import JSONResponse
 
+from lib.game.ChessGame import ChessGame
+
 router = APIRouter()
 
 @router.get("/newgame")
 async def game_endpoint():
     '''Setups a new game of chess to be played'''
-    return JSONResponse(content={"game_start": True})
+    game = ChessGame()
+    return JSONResponse(content={ "game_start": True, "board": game.board, "uuid": game.id })
